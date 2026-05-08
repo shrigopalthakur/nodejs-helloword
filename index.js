9d1307b278a1b92d580a138daef13b3177828fda
shrigopalthakur <164137058+shrigopalthakur@users.noreply.github.com>
2026-05-08 12:55:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write(' this is demo for automation for  new testing '); //write a response to the client
+  res.write(' this is demo for akfjdsfdfk.dfjdfdfkls'); //write a response to the client
   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
